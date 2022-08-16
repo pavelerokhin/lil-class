1c9a71761b9e7acae01ba889c8f9d22d15cf7d95
pavelerokhin <pavel.erokhin@rstor.io>
2022-08-16 19:23:16 +0200

    checkers

--- utl/ClassStat.go
+++ utl/ClassStat.go
@@ -2,12 +2,43 @@ package utl
 
 import (
 	"fmt"
+	"golang.org/x/net/html"
 	"sort"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 type ClassStats map[string]*Stats
+
+// NewClassStats is a constructor method
+func NewClassStats(s Selections) ClassStats {
+	cls := make(ClassStats)
+	var total = 0
+	for _, element := range s {
+		classes, exists := element.Attr("class")
+		if !exists {
+			continue
+		}
+
+		for _, c := range strings.Split(classes, " ") {
+			if cls.Has(c) {
+				cls[c].Freq++
+			} else {
+				cls.Add(c)
+				cls[c].Selection = element
+				cls[c].Freq = 1
+				if len(element.Nodes) > 0 && element.Nodes[0].Type == html.ElementNode {
+					cls[c].Type = element.Nodes[0].Data
+				}
+			}
+			total++
+		}
+	}
+	cls.SetTotal(total)
+
+	return cls
+}
 
 func (cs ClassStats) GetCommonParentWithin(deltaBetweenElements int) *goquery.Selection {
 	var commonParent *goquery.Selection
@@ -27,6 +58,27 @@ func (cs ClassStats) GetCommonParentWithin(deltaBetweenElements int) *goquery.Se
 	}
 	return nil
 }
+func (cs ClassStats) GetSufficientParents() Selections {
+	var parents, suff Selections
+	for _, s := range cs {
+		parent := s.GetSufficientParent()
+		if parent != nil {
+			suff = append(suff, parent)
+		}
+		parents = append(parents, parent)
+	}
+
+	// base cases for the recursion
+	if len(parents) == 0 {
+		return nil
+	}
+	if len(suff) == 0 {
+		cs2 := NewClassStats(parents)
+		return cs2.GetSufficientParents()
+	}
+
+	return suff
+}
 
 type Stats struct {
 	Freq      float64
@@ -35,16 +87,26 @@ type Stats struct {
 	Total     int
 }
 
+func (s *Stats) GetParent() *goquery.Selection {
+	return s.Selection.Parent()
+}
+
 func (s *Stats) GetParents() *goquery.Selection {
 	return s.Selection.Parents()
 }
 
-func (s *Stats) String() string {
-	return fmt.Sprintf("type: %s freq: %.0f total: %d", s.Type, s.Freq, s.Total)
+func (s *Stats) GetSufficientParent() *goquery.Selection {
+	var sufficientParent *goquery.Selection
+	parent := s.GetParent()
+	// check if parent is  sufficient
+	if parent != nil && IsParentSufficient(parent) {
+		sufficientParent = parent
+	}
+	return sufficientParent
 }
 
-func NewClassStats() ClassStats {
-	return make(ClassStats)
+func (s *Stats) String() string {
+	return fmt.Sprintf("type: %s freq: %.0f total: %d", s.Type, s.Freq, s.Total)
 }
 
 func (cs ClassStats) Add(c string) {
@@ -62,7 +124,6 @@ func (cs ClassStats) PrintInOrderAlpha() {
 		keys = append(keys, k)
 	}
 	sort.Strings(keys)
-
 	for _, k := range keys {
 		fmt.Printf("name: %s %s\n", k, cs[k].String())
 	}
@@ -70,14 +131,12 @@ func (cs ClassStats) PrintInOrderAlpha() {
 
 func (cs ClassStats) PrintInOrderFreq() {
 	keys := make([]string, 0, len(cs))
-
 	for key := range cs {
 		keys = append(keys, key)
 	}
 	sort.SliceStable(keys, func(i, j int) bool {
 		return cs[keys[i]].Freq < cs[keys[j]].Freq
 	})
-
 	for _, k := range keys {
 		fmt.Printf("name: %s %s\n", k, cs[k].String())
 	}
--- utl/funcs.go
+++ utl/funcs.go
@@ -2,13 +2,52 @@ package utl
 
 import (
 	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/net/html"
 )
 
-type ByTextLength []*goquery.Selection
+type ByTextLength Selections
 
 func (a ByTextLength) Len() int           { return len(a) }
 func (a ByTextLength) Less(i, j int) bool { return len(a[i].Text()) < len(a[j].Text()) }
 func (a ByTextLength) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+
+func BaseElementsFormDocument(d *goquery.Document) Selections {
+	return Elements(d, SELECTOR_BASE)
+}
+
+func GetClass(e *goquery.Selection) string {
+	class, exists := e.Attr("class")
+	if !exists {
+		class = "-"
+	}
+
+	return class
+}
+
+func GetType(e *goquery.Selection) string {
+	eType := "unknown"
+	if len(e.Nodes) > 0 && e.Nodes[0].Type == html.ElementNode {
+		eType = e.Nodes[0].Data
+	}
+
+	return eType
+}
+
+func IsParentSufficient(element *goquery.Selection) bool {
+	var score int
+	for _, check := range RegexCheck {
+		if !Has(element, check.Selector) {
+			continue
+		}
+		if check.Checker != nil && check.Checker(element) {
+			score++
+			continue
+		}
+		score++
+	}
+
+	return score > MANDATORY_SCORE
+}
 
 func Elements(d *goquery.Document, selector string) Selections {
 	var sub Selections
@@ -16,23 +55,13 @@ func Elements(d *goquery.Document, selector string) Selections {
 	s.Each(func(i int, e *goquery.Selection) {
 		sub = append(sub, e)
 	})
+
 	return sub
 }
 
-func SelectBaseElements(d *goquery.Document) Selections {
-	BASE_SELECTOR := "div a, div p"
-	return Elements(d, BASE_SELECTOR)
-}
-
-func SelectOnlyWithText(s Selections) Selections {
-	var sub Selections
-	for _, e := range s {
-		if len(e.Text()) > 0 {
-			sub = append(sub, e)
-		}
-	}
-
-	return sub
+func Has(element *goquery.Selection, selector string) bool {
+	selection := element.ChildrenFiltered(selector)
+	return selection != nil
 }
 
 func MaxInt(a, b int) int {
@@ -40,4 +69,14 @@ func MaxInt(a, b int) int {
 		return a
 	}
 	return b
+}
+
+func OnlyWithText(s Selections) Selections {
+	var sub Selections
+	for _, e := range s {
+		if len(e.Text()) > 0 {
+			sub = append(sub, e)
+		}
+	}
+	return sub
 }
